e80062ae920d75049e0fc9b0dcb76d3c04bec9ec
Joel Anderson <joelbanderson91@gmail.com>
2025-05-16 13:34:41 +0200

    pg 233

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
-def main():
-    print("Hello from alieninvasion!")
+import sys
+import pygame
+from settings import Settings
+
+
+class AlienInvasion:
+    """Overall class to manage game assets and behaviour."""
+
+    def __init__(self):
+        """Initialise game, create game resources."""
+        pygame.init()
+        self.clock = pygame.time.Clock()
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height)
+        )
+        pygame.display.set_caption("Alien Invasion")
+
+    def run_game(self):
+        """Start main loop for game."""
+        while True:
+            # Watch for keyboard and mouse events.
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            # Redraw screen during each pass through loop.
+            self.screen.fill(self.settings.bg_colour)
+            # Make most recently drawn screen visible.
+            pygame.display.flip()
+            self.clock.tick(60)
 
 
 if __name__ == "__main__":
-    main()
+    ai = AlienInvasion()
+    ai.run_game()
